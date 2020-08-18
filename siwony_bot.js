f9daf752df38e1292bed47118e7038f0fa1adb14
siwony_ <siwon103305@gmail.com>
2020-08-18 16:21:11 +0900

    bot_cat_say_hello

--- siwony_bot.js
+++ siwony_bot.js
@@ -1,16 +1,43 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();    //client 는 봇
-const PREFIX = "시워나"
+const PREFIX = "시워나 "
 const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('Logged in as ${client.user.tag}!');
 });
 
+//디코 명령어
 client.on('message', msg => {
-    if (msg.content === PREFIX + 'ping') {
-        msg.channel.send('ping');
+    //인삿말
+    if (msg.content === PREFIX + '하로' || msg.content === PREFIX + '안녕' || msg.content === PREFIX + '안뇽') {
+        let hello_rand_num = Math.floor(Math.random() * 3 ) + 1;
+        switch (hello_rand_num){
+            case 1 :
+                msg.channel.send('하로o//');
+                break;
+            case 2 :
+                msg.channel.send('안뇽!');
+                break;
+            case 3 :
+                msg.channel.send('안녕하세여ㅓ...!');
+                break;
+        }
+    }
+    //명령어가 없을떄
+    else{
+        let cmd_not_found = Math.floor(Math.random() * 2) + 1;
+        switch (cmd_not_found){
+            case 1 :
+                msg.channel.send('(갸웃..?');
+                break;
+            case 2 :
+                msg.channel.send('몰라여...^^ㅣ발...')
+                break;
+        }
+        return 
     }
 });
+
 
 client.login(config.token);
