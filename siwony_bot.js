006adbf90a7aeb01118789f10c6d8705408095c3
siwony_ <siwon103305@gmail.com>
2020-08-18 12:04:59 +0900

    siwony_bot_can_say_ping_pong

--- siwony_bot.js
+++ siwony_bot.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();    //client 는 봇
-const token = "NzQ0ODcwNTA2NDMxNTEyNjE4.Xzpgtw.UWLn6hqXjJ9FojiGJ4Z22422pe0";  //토큰
+const PREFIX = "시워나"
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('Logged in as ${client.user.tag}!');
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong!');
+    if (msg.content === PREFIX + 'ping') {
+        msg.channel.send('ping');
     }
 });
 
-client.login(token);
+client.login(config.token);
