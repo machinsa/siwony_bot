38d05fedb1de6cbae3aeb3e4198f06550aed6811
siwony_ <siwon103305@gmail.com>
2020-08-20 16:57:52 +0900

    구르기 기능, 드립 추가!

--- siwony_bot.js
+++ siwony_bot.js
@@ -2,17 +2,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();    //client 는 봇
 const PREFIX = "시워나 "
 const config = require('./config.json');
+const hello_li = ["하로", "안녕", "안뇽"];
 
+//로그인
 client.on('ready', () => {
     console.log('Logged in as ${client.user.tag}!');
+    //봇 ~하는중
+    client.user.setActivity('siwony_bot!', { type: 'PLAYING' })
 });
 
 //디코 명령어
 client.on('message', msg => {
     //인삿말
-    if (msg.content === PREFIX + '하로' || msg.content === PREFIX + '안녕' || msg.content === PREFIX + '안뇽') {
-        let hello_rand_num = Math.floor(Math.random() * 3 ) + 1;
-        switch (hello_rand_num){
+    let hello_idx = hello_li.indexOf(msg.content)
+    if (msg.content === PREFIX + hello_li[hello_idx]) {
+        let rand_num = Math.floor(Math.random() * 3 ) + 1; 
+        switch (rand_num){
             case 1 :
                 msg.channel.send('하로o//');
                 break;
@@ -24,19 +29,30 @@ client.on('message', msg => {
                 break;
         }
     }
-    //명령어가 없을떄
-    else{
-        let cmd_not_found = Math.floor(Math.random() * 2) + 1;
-        switch (cmd_not_found){
+    //구르기
+    else if(msg.content === PREFIX + "굴러" || msg.content === PREFIX + "굴러!"){
+        let rand_num = Math.floor(Math.random() * 2 ) + 1; 
+        switch (rand_num){
             case 1 :
-                msg.channel.send('(갸웃..?');
+                msg.channel.send("데구르르 쾅...!");
                 break;
             case 2 :
-                msg.channel.send('몰라여...^^ㅣ발...')
+                msg.channel.send("흐애ㅐ....");
+        }
+    }
+    //시워니 드립
+    else if(msg.content === PREFIX + "시원해" || msg.content === PREFIX + "시원스쿨"){
+        let rand_num = Math.floor(Math.random() * 2) + 1; 
+        switch (rand_num){
+            case 1 :
+                msg.channel.send(".....!!");
+                break;
+            case 2 :
+                msg.channel.send("놀리자마여어ㅓ....야발...");
                 break;
         }
-        return 
     }
+    //명령어가 없을떄
 });
 
 
