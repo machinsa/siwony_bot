364fe651ac3499a1e0c17c2918d432af208facc1
siwony_ <siwon103305@gmail.com>
2020-08-20 20:10:11 +0900

    도움말 라이츄 커맨드 추가

--- siwony_bot.js
+++ siwony_bot.js
@@ -18,18 +18,21 @@ client.on('message', msg => {
 
     let commend = msg.content.substring(PREFIX.length, msg.length);    //접두사 제외한 단어 자르기
     console.log(commend);
+    //도움말
+    if(commend === "도움말" || commend === "help" || commend === "커맨드"){
+        msg.reply('url........');
+    }
     //인삿말
-    if (commend === "하로" || commend === "안녕" || commend === "안뇽") {
-        let rand_num = Math.floor(Math.random() * 3 ) + 1; 
+    else if (commend === "하로" || commend === "안녕" || commend === "안뇽") {
+        let rand_num = Math.floor(Math.random() *3 ) + 1; 
         switch (rand_num){
-            case 1 :
-                msg.channel.send('하로o//');
+            case 1 : msg.channel.send('하로o//');
                 break;
-            case 2 :
-                msg.channel.send('안뇽!');
+            case 2 : msg.channel.send('안뇽!');
                 break;
-            case 3 :
-                msg.channel.send('안녕하세여ㅓ...!');
+            case 3 : msg.channel.send('안녕하세여ㅓ...!');
+                break;
+            case 4 : msg.channel.send('0ㅅ0');
                 break;
         }
     }
@@ -45,18 +48,45 @@ client.on('message', msg => {
         }
     }
     //시워니 드립
-    else if(commend === PREFIX + "시원해" || commend === PREFIX + "시원스쿨"){
-        let rand_num = Math.floor(Math.random() * 2) + 1; 
-        switch (rand_num){
+    else if(commend === "시원해" || commend === "시원스쿨"){
+        let rand_num = Math.floor(Math.random() * 3) + 1; 
+        switch (rand_3um){
             case 1 :
                 msg.channel.send(".....!!");
                 break;
             case 2 :
                 msg.channel.send("놀리자마여어ㅓ....야발...");
                 break;
+            case 3 : msg.channel.send('0ㅅ0');
+                break;
+3       }   
+    }
+    else if(commend === "라이츄"){
+        let rand_num = Math.floor(Math.random() * 4) + 1; 
+        switch (rand_num){
+            case 1 :
+                msg.channel.send("엄청나신분...!!");
+                break;
+            case 2 :
+                msg.channel.send("선배님...!");
+                break;
+            case 3 :
+                msg.channel.send("라이츄!");
+            case 4 :
+                msg.channel.send("겨울특강 없다고요....?");
         }
     }
-    //명령어가 없을떄
+    else{   //도움말이 없얼떄
+        let rand_num = Math.floor(Math.random() * 3) + 1; 
+        switch (rand_num){
+            case 1 : msg.channel.send(".....?");
+                break;
+            case 2 : msg.channel.send("(갸웃...?");
+                break;
+            case 3 : msg.channel.send("그게뭐에요....?");
+                break;
+        }
+    }
 });
 
 
