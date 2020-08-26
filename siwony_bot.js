d95713d25d8655a37d9327b1b2aa849553a3bf7d
siwony_ <siwon103305@gmail.com>
2020-08-26 23:07:15 +0900

    embed_learn start

--- siwony_bot.js
+++ siwony_bot.js
@@ -15,21 +15,25 @@ client.on('ready', () => {
 client.on('message', msg => {
     if(msg.author.bot){return}//봇이면 리턴
     else if(msg.content.indexOf(PREFIX) !== 0){return}//접두사 안붙이면 리턴
-
     let command = msg.content.substring(PREFIX.length, msg.length);    //접두사 제외한 단어 자르기
     console.log(command);
+    let user_request;   let bot_response;
     //도움말
     if(command === "도움말" || command === "help" || command === "커맨드"){
         const embed = new Discord.MessageEmbed()
             .setTitle("siwony_bot!(시워니봇)") // 여기는 임베드에서 타이틀로 사용됩니다!
-            .setDescription("siwony_ 가 js공부하고싶어 만든봇이에여 요거는 ") // 여기는 타이틀을 설명해주는 걸로 사용됩니다!
-            .setColor("0x87cefa") // 참고로 색은 헥사 넣으면 안되고 그 걍 나한테 물어봐 여기는 색상을 설정하는 공간인데 HEX값을 넣으셔도 됩니다! (#7239DA) "red" 말고 다른것들도 있어요! 맨 밑에다가 적어놓을테니 확인해주세요!
-            .setFooter("밑부분") // 여기는 임베드의 밑부분에서 말머리로 사용됩니다!
-            //.setThumbnail("http://blogfiles.naver.net/20151023_23/shin_0305_1445573936921jrPRT_JPEG/%BD%E6%B3%D7%C0%CF%BF%B9%BD%C3.jpg") // 여기는 임베드에서 썸네일로 불려옵니다! (URL를 넣어가 경로를 기입하면 그 경로에 있는 이미지를 불러와 썸네일로 이용되요!
+            .setColor("0x87cefa") //임베드 색
+            .setDescription("siwony_ 가 js공부하고싶어 만든봇이에여 요거는 지속적으로 업데이트를 할꺼에여"
+                + " 그냥 js공부하려고 하는거니까 뭐... 지속적으로 업뎃할꺼같은데 아무튼 모르겠다아ㅏㅏ ㅎㅅㅎ") // 여기는 타이틀을 설명해주는 걸로 사용됩니다! 
+            .setFooter("siwony_") // 여기는 임베드의 밑부분에서 말머리로 사용됩니다!
+            .setThumbnail("https://cdn.discordapp.com/avatars/366087289190875137/d551b5b1e76dc995a6d03bfc9fcc67c0.png?size=128") // 여기는 임베드에서 썸네일로 불려옵니다! (URL를 넣어가 경로를 기입하면 그 경로에 있는 이미지를 불러와 썸네일로 이용되요!
             //.setImage("url") // 여기는 임베드에서 이미지로 사용되는 곳입니다. // 위에 설명이랑 같아요
-        
-            .setTimestamp() // 여기는 타임스탬프를 설정하는 공간인데 비워두면 현재시각, 여기에 타임스탬프를 넣으시면 그 값에 맞는 시간으로 변환됩니다(라이츄 바보 자식)
-            .addField("여기는 소제목", "여기는 소제목 설명같은거...?") // 첫번째 칸은 임베드의 소제목, 두번째 칸은 임베드의 소제목의 설명하는 공간입니다! 세번째 칸은 INLINE으로 사용되는데 TRUE 하면 라인에 들어가는거고 FALSE 하면 밑라인으로 내려가게 됩니다.
+            .setTimestamp() // 여기는 타임스탬프를 설정하는 공간인데 비워두면 현재시각, 여기에 타임스탬프를 넣으시면 그 값에 맞는 시간으로 변환됩니다
+            .addField("접두사","시워나 [커멘드] 를 하면 봇이 작동해요!")
+            .addField("인사(안녕, 하로, 안뇽)", "siwony가 인사를 해줘요...!") // 첫번째 칸은 임베드의 소제목, 두번째 칸은 임베드의 소제목의 설명하는 공간입니다! 세번째 칸은 INLINE으로 사용되는데 TRUE 하면 라인에 들어가는거고 FALSE 하면 밑라인으로 내려가게 됩니다.
+            .addField("구르기(굴러)", "siwony가 굴러여어ㅓ...(데구르르...)")
+            .addField("야발", "시워니가 야발을 외쳐요!")
+            .addField("배워 [명령어]:[시워니가 배울말]", "시워니가 무려 학습을해요..!!!(대충 실험적이고 실행 안되는것)")
         msg.reply(embed) // EMBED를 REPLY로 답변합시다!
     }
     //인삿말
@@ -61,9 +65,12 @@ client.on('message', msg => {
         let rand_num = Math.floor(Math.random() * fuxx.length ); 
         msg.channel.send(fuxx[rand_num]);
     }
+    else if(command === `배워 ${user_request}:${bot_response}`){
+        
+    }
     else{   //도움말이 없얼떄
-        let none_command = ['.....?', '(갸웃...?', '그게뭐에요....?'];
-        let rand_num = Math.floor(Math.random() * none_command.length ); 
+        let none_command = ['.....?', '(갸웃...?', '그게뭐에요....?', '그게뭔대 씹덕아ㅏ!'];
+        let rand_num = Math.floor(Math.random() * none_command.length );
         msg.channel.send(none_command[rand_num]);
     }
 });
